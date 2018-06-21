83b0216fc0d36a883cbceb97ee5fe5725ee2d41c
Yolk Huang <yolk.tw@gmail.com>
2018-06-22 00:44:36 +0800

    Add definedemo.c

---
+++ definedemo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define x 100
+#define y 200
+
+int main(void) {
+  printf("常數 x = %d, y = %d \n", x, y);
+
+  printf("Pree any key to continue... \n");
+  getchar();
+
+  return 0;
+}
