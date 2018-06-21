48f58eed725528f6bf2edeb422dd85f33631e4d0
Yolk Huang <yolk.tw@gmail.com>
2018-06-21 18:54:03 +0800

    Add dzero.c

---
+++ dzero.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int x = 100;
+  int y = 0;
+
+  printf("%d \n", x/y);
+
+  printf("Press any key to continue...\n");
+  getchar();
+
+  return 0;
+}
