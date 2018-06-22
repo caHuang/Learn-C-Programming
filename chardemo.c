48a99ce210e616082c2dfae370912930ecac9038
Yolk Huang <yolk.tw@gmail.com>
2018-06-23 00:12:36 +0800

    Add chardemo.c

---
+++ chardemo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  char c1 = 'x';
+  char c2 = 'y';
+
+  printf("char 變數 c1 = %c \n", c1);
+  printf("char 變數 c2 = %c \n", c2);
+
+  printf("Pree any key to continue... \n");
+  getchar();
+
+  return 0;
+}
